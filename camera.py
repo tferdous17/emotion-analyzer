b9a3c59c51d165241a138b83e841d696ca0eaba7
Gagan <gagansapkota668@gmail.com>
2023-11-10 18:44:31 -0500

    It records a Video and Saves it

--- camera.py
+++ camera.py
@@ -2,12 +2,22 @@ import cv2
 import time
 import datetime
 
-cap = cv2.VideoCapture(1) # will capture the camera, 1 signifies its my macbook cam
+cap = cv2.VideoCapture(0) # will capture the camera, 1 signifies its my macbook cam
 
 # sets up a cascade classifier using a pre-existing one called haarcascade
 # haarcascade is prebuilt into opencv
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalFace_default.xml")
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
+recording = True
+#This gets the width and the hight of the frame size of the video
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+#This records the video in mp4v
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+#Output Steam, the 20 is the frame rate,
+out = cv2.VideoWriter("video.mp4", fourcc, 20.0, frame_size)
+
 
 while True:
     _, frame = cap.read() # read
@@ -18,6 +28,13 @@ while True:
     #   - dont worry about the num 5: its using the min neighbors algo
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     bodies = body_cascade.detectMultiScale(gray, 1.3, 2)
+
+    #it will record when the number of faces and bodies increases
+    if len(faces) + len(bodies) > 0:
+        recording = True
+
+    #code to record the video
+    out.write(frame)
 
     for (x, y, width, height) in faces:
         cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
@@ -32,5 +49,7 @@ while True:
     if cv2.waitKey(1) == ord('q'): # q is the key to exit the cam
         break
 
+#this will save the video
+out.release()
 cap.release()
 cv2.destroyAllWindows() # when while loop breaks, destroy all "windows" aka the window
