10ebd2975f07347dc52353c1150a7f2664944e97
Tasnim Ferdous <tferdous611@gmail.com>
2023-11-10 14:52:39 -0500

    import views and register blueprint

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "this is the home page"
+app.register_blueprint(views, url_prefix="/analyzer")
 
 if __name__ == '__main__':
     app.run(debug=True)
