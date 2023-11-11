c8f2557d19cf7aff4267ae7b408cd315e55a906d
Tasnim Ferdous <tferdous611@gmail.com>
2023-11-10 20:35:23 -0500

    use mediapipe to draw landmarks for facial & body recognition

--- camera.py
+++ camera.py
@@ -1,55 +1,50 @@
+import mediapipe as mp
 import cv2
-import time
-import datetime
 
-cap = cv2.VideoCapture(0) # will capture the camera, 1 signifies its my macbook cam
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
 
-# sets up a cascade classifier using a pre-existing one called haarcascade
-# haarcascade is prebuilt into opencv
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalFace_default.xml")
-body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2)
 
-recording = True
-#This gets the width and the hight of the frame size of the video
-frame_size = (int(cap.get(3)), int(cap.get(4)))
-#This records the video in mp4v
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-
-#Output Steam, the 20 is the frame rate,
-out = cv2.VideoWriter("video.mp4", fourcc, 20.0, frame_size)
+cap = cv2.VideoCapture(1) # cap means "capture", so the video we're capturing from the webcam
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened(): ##
+        ret, frame = cap.read()
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        # Make Detections
+        results = holistic.process(image)
 
 
-while True:
-    _, frame = cap.read() # read
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY) # give us a grayscale img
+        # draw face "landmarks": print the contour of the analyzed face thru webcam
+        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                                  mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                                  mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                                  )
 
-    # DISCLAIMER: this is a premade algo:
-    #   -gray is the img, 1.3 is scale factor (accuracy of this pre-existing algo)
-    #   - dont worry about the num 5: its using the min neighbors algo
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    bodies = body_cascade.detectMultiScale(gray, 1.3, 2)
+        # draw landmarks for right hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                                  )
 
-    #it will record when the number of faces and bodies increases
-    if len(faces) + len(bodies) > 0:
-        recording = True
+        # draw landmarks for left hand
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                                  )
 
-    #code to record the video
-    out.write(frame)
+        # draw landmarks for pose (full body)
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  )
 
-    for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
 
+        cv2.imshow('Webcam Feed', image)
 
-    for x, y, width, height in bodies:
-        pad_w, pad_h = int(0.15*width), int(0.05*height)
-        cv2.rectangle(frame, (x + pad_w, y + pad_h), (x + width - pad_w, y + height - pad_h), (0,255,0), 1)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
-    cv2.imshow("Camera", frame) # show the camera
-
-    if cv2.waitKey(1) == ord('q'): # q is the key to exit the cam
-        break
-
-#this will save the video
-out.release()
 cap.release()
-cv2.destroyAllWindows() # when while loop breaks, destroy all "windows" aka the window
+cv2.destroyAllWindows()
