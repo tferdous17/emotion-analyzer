b8ffbf0cb0deaa0ce3a0b0738c14bb37d02e17ee
Tasnim Ferdous <tferdous611@gmail.com>
2023-11-10 21:42:13 -0500

    last min modification

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from views import views
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/analyzer")
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
